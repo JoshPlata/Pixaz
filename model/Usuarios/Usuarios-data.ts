f518efa556367ee4f4e0674463c5f90c1a0c709e
christianrv007 <eduardorv07@hotmail.com>
2020-10-29 14:49:18 -0600

    usuarios

---
+++ model/Usuarios/Usuarios-data.ts
@@ -0,0 +1,27 @@
+import { Usuarios } from './Usuario';
+export const UsuariosData:Usuarios[]=[
+    {
+        id_Usuario:1,
+        nombre_Usuario:"Lalito ",
+        correo:"lalito@gmail.com",
+        telefono:5512345,
+        direccion:"las milpas 3",
+        tipo:"cliente",
+    },
+    {
+        id_Usuario:2,
+        nombre_Usuario:"plata",
+        correo:"plata@gmail.com",
+        telefono:553344335,
+        direccion:"los rosales izquierdos",
+        tipo:"trabajador",
+    },
+    {
+        id_Usuario:3,
+        nombre_Usuario:"Jair",
+        correo:"jair@gmail.com",
+        telefono:5534545,
+        direccion:"izalli cofra 3",
+        tipo:"cliente",
+    }
+]
